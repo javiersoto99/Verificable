dcf5cdd496438e7d1943eafb5dd3298aaee377ff
javiersoto99] <jisoto2@miuandes.cl>
2023-03-21 20:11:10 -0300

    first commit

---
+++ UAndes.ICC5103.202301-master/UAndes.ICC5103.202301/Controllers/InscripcionesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UAndes.ICC5103._202301.Controllers
+{
+    public class InscripcionesController : Controller
+    {
+        // GET: Inscripciones
+        public ActionResult Inscripciones()
+        {
+            return View();
+        }
+
+        public ActionResult NuevaInscripcion()
+        {
+            return View();
+        }
+    }
+}
