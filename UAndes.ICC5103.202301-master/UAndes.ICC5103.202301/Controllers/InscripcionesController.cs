e4d454eb9df82095d429221ca714c39c224add0f
javiersoto99] <jisoto2@miuandes.cl>
2023-03-23 16:30:10 -0300

    nueva pantalla

--- UAndes.ICC5103.202301-master/UAndes.ICC5103.202301/Controllers/InscripcionesController.cs
+++ UAndes.ICC5103.202301-master/UAndes.ICC5103.202301/Controllers/InscripcionesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using UAndes.ICC5103._202301.Models;
@@ -17,8 +18,35 @@ namespace UAndes.ICC5103._202301.Controllers
             return View();
         }
 
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ListarInscripciones()
+        {
+
+            return View();
+        }
+
         // GET: NuevaInscripcion
         public ActionResult NuevaInscripcion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> GuardarInscripcion()
+        {
+            return View();
+        }
+
+        // GET: VerInscripcion
+        public ActionResult VerInscripcion()
+        {
+            return View();
+        }
+
+        // GET: BuscarInscripcion
+        public ActionResult BuscarInscripcion()
         {
             return View();
         }
